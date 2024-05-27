7070c374336f34cdfec6dba27a83ef3926c34ecb
yoouung <jyp101400@egmail.com>
2024-05-28 01:53:08 +0900

    refactor: refactor Thread run code

--- src/main/java/subway/management/Manager.java
+++ src/main/java/subway/management/Manager.java
@@ -2,6 +2,9 @@ package subway.management;
 
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Manager implements Runnable {
     private static final int MAX_OUT_OF_STOCK_ITEMS = 3;
@@ -17,23 +20,26 @@ public class Manager implements Runnable {
 
     @Override
     public void run() {
-        while (outOfStockItemsCount < MAX_OUT_OF_STOCK_ITEMS) {
-            try {
-                Thread.sleep(5000 + random.nextInt(15000)); // 5~20초 랜덤 대기
+        int initialDelay = 5;
+        int period = random.nextInt(16); // 5~20초 랜덤 간격
 
-                int index = random.nextInt(items.size()); // 재고가 소진된 항목을 랜덤으로 선택
-                String item = items.get(index);
-
-                if (inventory.isInStock(item)) { // 재고가 소진되지 않은 항목인 경우
-                    inventory.setStockStatus(item);
-                    outOfStockItemsCount++;
-                    System.out.println();
-                    System.out.println("❗️알립니다: " + item + " 재고가 소진되었습니다.");
-                    System.out.println();
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(() -> {
+            if (outOfStockItemsCount >= MAX_OUT_OF_STOCK_ITEMS) {
+                scheduler.shutdown();
+                return;
             }
-        }
+
+            int index = random.nextInt(items.size()); // 재고가 소진된 항목을 랜덤으로 선택
+            String item = items.get(index);
+
+            if (inventory.isInStock(item)) { // 재고가 소진되지 않은 항목인 경우
+                inventory.setStockStatus(item);
+                outOfStockItemsCount++;
+                System.out.println();
+                System.out.println("❗️알립니다: " + item + " 재고가 소진되었습니다.");
+                System.out.println();
+            }
+        }, initialDelay, period, TimeUnit.SECONDS);
     }
 }
